0e9ae054443f9ae4b7c15bd3a7471fdc6d0310fc
RoniRamon <roniramon260@gmail.com>
2018-06-26 17:08:53 -0700

    add routes

---
+++ src/components/posts_index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PostsIndex extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Posts Index 
+      </div>
+    );
+  }
+}
+
+export default PostsIndex;
