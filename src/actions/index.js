096f380923785ea89e36e2f72762fa3bd6d70766
RoniRamon <roniramon260@gmail.com>
2018-06-27 21:23:59 -0700

    add axios - action to fetch information from the api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
+import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 
-export const fetchPosts = () => ({
-  type: FETCH_POSTS
-});
+const ROOT_URL ='http://reduxblog.herokuapp.com/api/';
+const API_KEY = '?key=CODYR12345';
+
+export const fetchPosts = () => {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+  return {
+    type: FETCH_POSTS,
+    payload: request,
+  };
+};
