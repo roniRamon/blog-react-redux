cb449741e9fd5adc27b19af7beb3c0e60fb22f40
RoniRamon <roniramon260@gmail.com>
2018-06-29 16:37:25 -0700

    lecture 127

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
 
   render() {
     return (
       <div>
-        Posts Index 
+        Posts Index
       </div>
     );
   }
 }
 
-export default PostsIndex;
+export default connect(null, {fetchPosts})(PostsIndex);
