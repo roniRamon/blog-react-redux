ffd1ab97ffc20650080f6032e056db76f89ed25d
RoniRamon <roniramon260@gmail.com>
2018-06-29 17:03:11 -0700

    new component posts new to create new posts

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PostsNew extends React.Component {
+
+  render() {
+    return (
+      <div>
+        PostsNew!
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
