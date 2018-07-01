bd4b0d18c87ff1a8f1265941e0dba00c228a5580
RoniRamon <roniramon260@gmail.com>
2018-06-30 20:41:49 -0700

    add redux-form to handle crete new post form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
-
+import { reducer as formReducer } from 'react-form';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
+  form: formReducer,
 });
 
 export default rootReducer;
